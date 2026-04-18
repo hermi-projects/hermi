69629e5e088bc636846719dbf961491615ac62a9
hermi-projects <=>
2026-04-17 21:53:49 -0400

    feat: add HermiRequired annotation for mandatory log fields

---
+++ hermi-logging/src/main/java/org/hermi/logging/annotations/HermiRequired.java
@@ -0,0 +1,7 @@
+package org.hermi.logging.annotations;
+
+/**
+ * Indicates that the annotated field is required when a log event is started, succeeded, or results
+ * in a warning.
+ */
+public @interface HermiRequired {}
